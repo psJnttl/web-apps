50fde313e5332d4cfe2560d9e75139f1dae14aed
Pasi Junttila <pasi.junttila@live.com>
2017-05-01 14:42:48 +0300

    Create V2__populate_test_data.sql

---
+++ employees/src/main/resources/db/migration/V2__populate_test_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO employee (first_name, family_name, city, employer, salary, title) VALUES
+('Anne','Virtanen', 'Tampere', 'Solita', 2988.7, 'järjestelmävalvoja'),
+('Aapo', 'Nieminen', 'Oulu', 'Solteq', 3599.6, 'systeemiarkkitehti'),
+('Jarmo', 'Korhonen', 'Helsinki', 'Sonera', 5055.3, 'ohjelmistokehittäjä'),
+('Sanna', 'Saarinen', 'Tampere', 'Sovelto',4503.1, 'projektipäällikkö'),
+('Harri', 'Vatanen', 'Oulu', 'Solita', 3988.3, 'ohjelmistokehittäjä'),
+('Sofia', 'Salonen', 'Tampere', 'Solita', 4375.8, 'projektipäällikkö'),
+('Mikael', 'Stenros', 'Helsinki', 'Sonera', 4321.9, 'firmamyynti'),
+('Jari', 'Pihlajamäki', 'Tampere', 'Sonera', 4211.3, 'myyntipäällikkö'),
+('Milla', 'Kataja', 'Oulu', 'Solteq', 3987.1, 'laatupäällikkö'),
+('Pauli', 'Nevala', 'Tampere', 'Solita', 4770.23, 'asiakkuuspäällikkö');
+
+INSERT INTO employer (name, y_code) VALUES
+('Atostek', '1571997-4'),
+('Futurice', '1623507-4 '),
+('Reaktor', '2447229-9'),
+('Solita', '1060155-5'),
+('Solteq', '0490484-0'),
+('Sonera', '1475607-9'),
+('Sovelto', '0566575-7');
