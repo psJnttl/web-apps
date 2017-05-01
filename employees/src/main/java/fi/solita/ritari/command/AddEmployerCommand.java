acce26519c3b44bb925b3a5be6d9abee1ff318bc
Pasi Junttila <pasi.junttila@live.com>
2017-05-01 14:24:18 +0300

    Add files via upload

---
+++ employees/src/main/java/fi/solita/ritari/command/AddEmployerCommand.java
@@ -0,0 +1,14 @@
+package fi.solita.ritari.command;
+
+public class AddEmployerCommand {
+    private String name;
+    private String yCode;
+    
+	public String getName() {
+		return name;
+	}
+	public String getyCode() {
+		return yCode;
+	}
+    
+}
